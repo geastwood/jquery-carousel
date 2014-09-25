5eca2b7ac1ff85b866b553414eed07405c210283
Fei Liu <liu.feiwood@gmail.com>
2014-09-25 17:59:34 +0200

    bug fix

--- dist/carousel.js
+++ dist/carousel.js
@@ -88,13 +88,13 @@ animations = {
 // controls the logic of animation
 // easing: easeOutBack, easeOutBounce, easeOutElastic, easeInExpo
 animator = function (type, opts) {
-  var duration = opts.duration || 400, anim = animations[type](opts.direction, { orientation: this.cfg.animationOrientation }), elLocator = this.cfg.elLocator, exitItems = this.queue.exit(opts.direction), enterItems = this.queue.enter(opts.direction);
+  var that = this, duration = opts.duration || 400, anim = animations[type](opts.direction, { orientation: this.cfg.animationOrientation }), elLocator = this.cfg.elLocator, exitItems = this.queue.exit(opts.direction);
   $.each(exitItems, function (i, item) {
     var el = item[elLocator]('a');
     el.css({ float: 'left' }).stop().animate(anim.out(el), duration, function () {
       // make sure only to call `enter` once, only call enter when all `out` are finished
       if (exitItems.length === i + 1) {
-        $.each(enterItems, function (j, item) {
+        $.each(that.queue.enter(opts.direction), function (j, item) {
           var el = item[elLocator]('a');
           el.css(anim.initial(el)).stop().animate(anim['in'](el), duration, anim.easing('easeOutBounce'));
         });
